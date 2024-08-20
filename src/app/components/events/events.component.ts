a520a61121ad4852ddce6f865016f42fdfa438c1
Artifique <fombadaouda72@gmail.com>
2024-08-20 10:50:38 +0000

    latest

--- src/app/components/events/events.component.ts
+++ src/app/components/events/events.component.ts
@@ -7,6 +7,7 @@ import { AuthService } from 'src/app/service/auth.service';
 import { EventServiceService } from 'src/app/service/event-service.service';
 import { forkJoin, map } from 'rxjs';
 import { NgFor, NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-events',
@@ -19,12 +20,15 @@ import { NgFor, NgIf } from '@angular/common';
     RouterLink,
     NgIf,
     NgFor,
-    NavbarComponent
+    NavbarComponent,
+    FormsModule
   ],
 })
 export class EventsComponent implements OnInit {
+  EVENTE: string = '';
   user: Utilisateur | null = null;
   event: Evenement[] = [];
+  filteredEvents: Evenement[] = [];
   prixMap: Map<number, number> = new Map(); // Map pour stocker les prix associés aux événements
 
   constructor(
@@ -40,7 +44,6 @@ export class EventsComponent implements OnInit {
     this.eventService.getEvents().subscribe(
       (events: Evenement[]) => {
         this.event = events;
-        console.log("events:", events);
 
         // Récupérer les prix pour chaque événement
         const priceObservables = this.event.map(event =>
@@ -54,15 +57,26 @@ export class EventsComponent implements OnInit {
           prices.forEach(priceData => {
             this.prixMap.set(priceData.eventId, priceData.prix); // Stocker les prix dans le Map
           });
-          console.log("events with prices:", this.prixMap);
+
+          // Appliquer les filtres après que tous les prix soient récupérés
+          this.filterEvents();
         });
       }
+    );
+  }
+
+  filterEvents() {
+    this.filteredEvents = this.event.filter(event =>
+      event.nom.toLowerCase().includes(this.EVENTE.toLowerCase()) ||
+      event.description.toLowerCase().includes(this.EVENTE.toLowerCase()) ||
+      event.lieu.toLowerCase().includes(this.EVENTE.toLowerCase())
     );
   }
 
   getPrixForEvent(eventId: number): number | undefined {
     return this.prixMap.get(eventId);
   }
+
   Deconnexion(){
     this.authService.clearUser();
     this.router.navigate(['/home']);
